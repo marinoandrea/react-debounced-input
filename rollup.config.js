b7f1f68e70a432749a0dc26a3229d50cf32f33ef
Andrea Marino <am.marinoandrea@gmail.com>
2023-05-29 11:35:16 +0200

    build: updated rollup config for peer deps

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,8 @@ import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import { terser } from "rollup-plugin-terser";
 
 const packageJson = require("./package.json");
 
@@ -21,9 +23,11 @@ export default [
       },
     ],
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      terser(),
     ],
   },
   {
