2dc98bc8e2935f0ecdd60de714c4654ea29fe2d7
Andrea Marino <am.marinoandrea@gmail.com>
2023-05-29 13:58:00 +0200

    test: add simple render tests and simplify folder structure

---
+++ __tests__/index.test.tsx
@@ -0,0 +1,63 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { DebouncedInput } from "../src";
+
+const DEBOUNCE_MS = 500;
+const PLACEHOLDER_TEXT = "debounced-input";
+
+function sleep(delay: number) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, delay);
+  });
+}
+
+describe("DebouncedInput", () => {
+  it("renders without setting the timeout", () => {
+    render(<DebouncedInput placeholder={PLACEHOLDER_TEXT} />);
+    const domNode = screen.getByPlaceholderText(PLACEHOLDER_TEXT);
+    expect(domNode).toBeTruthy();
+  });
+
+  it("renders with custom set timeout", () => {
+    render(
+      <DebouncedInput placeholder={PLACEHOLDER_TEXT} debounceMs={DEBOUNCE_MS} />
+    );
+    const domNode = screen.getByPlaceholderText(PLACEHOLDER_TEXT);
+    expect(domNode).toBeTruthy();
+  });
+
+  it("does not fire onChange before set timeout", async () => {
+    let hasChanged = false;
+
+    render(
+      <DebouncedInput
+        placeholder={PLACEHOLDER_TEXT}
+        debounceMs={DEBOUNCE_MS}
+        onChange={() => (hasChanged = true)}
+      />
+    );
+
+    const domNode = screen.getByPlaceholderText(PLACEHOLDER_TEXT);
+    fireEvent.change(domNode, { target: { value: "a" } });
+
+    await sleep(DEBOUNCE_MS - 100);
+    expect(hasChanged).toBe(false);
+  });
+
+  it("fires onChange after set timeout", async () => {
+    let hasChanged = false;
+
+    render(
+      <DebouncedInput
+        placeholder={PLACEHOLDER_TEXT}
+        debounceMs={DEBOUNCE_MS}
+        onChange={() => (hasChanged = true)}
+      />
+    );
+
+    const domNode = screen.getByPlaceholderText(PLACEHOLDER_TEXT);
+    fireEvent.change(domNode, { target: { value: "a" } });
+
+    await sleep(DEBOUNCE_MS + 100);
+    expect(hasChanged).toBe(true);
+  });
+});
